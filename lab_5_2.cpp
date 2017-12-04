df908b7f01c94a1199a9f5696fa4f6f06b9d83d5
Unknown <whatcharawali17@gmail.com>
2017-12-04 18:10:01 +0700

    แก้ข้อที่ 2 3

--- lab_5_2.cpp
+++ lab_5_2.cpp
@@ -12,10 +12,13 @@ int main() {
     cin >> name1;
 
     // แสดงชื่อ โดยตัดให้เหลือแค่ 10 ตัวอักษร
-    if (name1.length() > 10) {
-        cout << "Your name is too long!!!!" << endl;
-        name1.erase (name1.begin()+10, name1.end());
-        cout << "Your name has been changed to " << name1 << endl;
+    if (name1.length() > 10){
+            cout << "Your name is too long!!!!" << endl;
+            name1.erase (name1.begin()+10, name1.end());
+            cout << "Your name has been changed to " << name1 << endl;
+    }
+    if (name1.length() < 10){
+        cout << name1 << endl;
     }
 
     return 0;
